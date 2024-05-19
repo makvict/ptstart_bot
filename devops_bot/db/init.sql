3cbd9b09806936c5fd4d889f417c8f851bf5e623
makvict <168142259+makvict@users.noreply.github.com>
2024-05-19 03:02:10 +0300

    Update init.sql

--- devops_bot/db/init.sql
+++ devops_bot/db/init.sql
@@ -7,6 +7,6 @@ id SERIAL PRIMARY KEY,
 phone VARCHAR(255) NOT NULL
 );
 INSERT INTO emails(email) VALUES ('test@mail.ru');
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'Qq12345';
-SELECT pg_create_physical_replication_slot('replication_slot');
-GRANT pg_read_all_data TO replicator; GRANT pg_write_all_data TO replicator;
+create user ${DB_REPL_USER} with replication encrypted password ${DB_REPL_PASSWORD};
+select pg_create_physical_replication_slot('replication_slot');
+GRANT pg_read_all_data TO ; GRANT pg_write_all_data TO replicator;

b276bffd84d8661aada136ab970dbcf9f1376c0e
makvict <168142259+makvict@users.noreply.github.com>
2024-05-20 22:52:06 +0300

    Update init.sql

--- ansible/init.sql
+++ ansible/init.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE database;
+\connect database
 CREATE TABLE IF NOT EXISTS emails(
 id SERIAL PRIMARY KEY,
 email VARCHAR(255) NOT NULL
@@ -8,9 +10,8 @@ phone VARCHAR(255) NOT NULL
 );
 INSERT INTO emails(email) VALUES ('test@mail.ru');
 INSERT INTO phones(phone) VALUES ('89999999999');
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'Qq12345';
-SELECT pg_create_physical_replication_slot('replication_slot');
-GRANT pg_read_all_data TO replicator; GRANT pg_write_all_data TO replicator;
-
-CREATE USER postgres WITH REPLICATION ENCRYPTED PASSWORD 'Qq12345';
-GRANT pg_read_all_data TO postgres; GRANT pg_write_all_data TO postgres;
+create user DB_REPL_USER with replication encrypted password 'DB_REPL_PASSWORD';
+select pg_create_physical_replication_slot('replication_slot');
+GRANT pg_read_all_data TO DB_REPL_USER; GRANT pg_write_all_data TO DB_REPL_USER;
+CREATE USER DB_USER WITH with replication encrypted password 'DB_PASSWORD';
+GRANT pg_read_all_data TO DB_USER; GRANT pg_write_all_data TO DB_USER;
